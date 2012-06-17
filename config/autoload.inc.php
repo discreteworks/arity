b2242d1cb987f4c56e5b9cb990dd6bc126ad3c89
Shahrukh Hussain <algorist@yahoo.com>
2012-06-18 01:51:24 +0500

    disabled directory listing

--- config/autoload.inc.php
+++ config/autoload.inc.php
@@ -11,11 +11,7 @@ function __autoload($class_name) {
 		require_once dirname(__FILE__)."/../model/" . strtolower($class_name) . ".php";
 
 	}
-	if (file_exists(dirname(__FILE__)."/../res/" . strtolower($class_name) . ".php")) {
-
-		require_once dirname(__FILE__)."/../res/" . strtolower($class_name) . ".php";
-	}
-
+	
 	if (file_exists(dirname(__FILE__)."/../model/" . strtolower($class_name) . ".php")) {
 
 		require_once dirname(__FILE__)."/../model/" . strtolower($class_name) . ".php";
@@ -29,6 +25,12 @@ function __autoload($class_name) {
 	if (file_exists(dirname(__FILE__)."/../core/" . strtolower($class_name) . ".php")) {
 
 		require_once dirname(__FILE__)."/../core/" . strtolower($class_name) . ".php";
+	}
+	
+	/* Comment to disable test case object loading in production */
+	if (file_exists(dirname(__FILE__)."/../res/" . strtolower($class_name) . ".php")) {
+	
+		require_once dirname(__FILE__)."/../res/" . strtolower($class_name) . ".php";
 	}
 
 }
